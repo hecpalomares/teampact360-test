5b129ac6634a55cb34ddde264314564e9ed68c18
hecpalomares <hpalomares12@gmail.com>
2017-09-25 00:23:02 -0500

    Firebase connect. Display some questions from Firebase. Test Template v0.1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+  <div id="app" class="container">
+    <div class="page-header">
+      <h1>Teampact360 Assessment</h1>
+    </div>
+    <div class="panel-heading">
+      <h3 class="panel-title">Question List</h3>
+      <div class="panel-body">
+        <table class="table table-striped" v-for="question in questions">
+            <thead>
+              <tr>
+                <th>{{question['.key']}}</th>
+                <tr v-for="cluster in question">
+                  {{cluster}}
+                <tr>
+              </tr>
+            </thead>
+            <tbody>
+              <tr >
+              </tr>
+            </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-import Hello from './components/Hello'
+  <script>
+    import Firebase from 'firebase'
+    let config = {
+      apiKey: 'AIzaSyAuAqG-Zw7UANCBUeDc8zMzSeEASJCYdHk',
+      authDomain: 'teampact360-test.firebaseapp.com',
+      databaseURL: 'https://teampact360-test.firebaseio.com',
+      projectId: 'teampact360-test',
+      storageBucket: 'teampact360-test.appspot.com',
+      messagingSenderId: '66366476103'
+    }
 
-export default {
-  name: 'app',
-  components: {
-    Hello
-  }
-}
-</script>
+    let app = Firebase.initializeApp(config)
+    let db = app.database()
+    let questionsRef = db.ref('questions')
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    export default {
+      name: 'app',
+      firebase: {
+        questions: questionsRef
+      }
+    }
+  </script>
